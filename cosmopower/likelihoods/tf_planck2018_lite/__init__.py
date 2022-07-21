98817fedffd21ff3c96cb5898a6661d500dacad8
sophuang <109146037+sophuang@users.noreply.github.com>
2022-07-21 21:07:14 +0100

    Update __init__.py

--- cosmopower/likelihoods/tf_planck2018_lite/__init__.py
+++ cosmopower/likelihoods/tf_planck2018_lite/__init__.py
@@ -1,4 +1,5 @@
-from .tf_planck2018_lite import *
+from .tf_planck2018_lite import tf_planck2018_lite
+from .tf_planck2018_lite import tf_planck2018_lite_posterior
 
 __version__ = "0.1.0"
 __author__ = 'Alessio Spurio Mancini'
