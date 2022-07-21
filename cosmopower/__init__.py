3863adf54646b02faffcd441378f0fb811dd13e8
sophuang <109146037+sophuang@users.noreply.github.com>
2022-07-21 21:55:30 +0100

    Update __init__.py

--- cosmopower/__init__.py
+++ cosmopower/__init__.py
@@ -1,7 +1,7 @@
 from .cosmopower_PCA import cosmopower_PCA
 from .cosmopower_PCAplusNN import cosmopower_PCAplusNN
 from .cosmopower_NN import cosmopower_NN
-from .likelihoods import *
+from .likelihoods import tf_planck2018_lite,tf_planck2018_lite_posterior
 
 __version__ = "0.1.0"
 __author__ = 'Alessio Spurio Mancini'
