42e2b6ff8d94fca079622e67bda4950f1ad2a882
sophuang <109146037+sophuang@users.noreply.github.com>
2022-07-31 14:32:49 +0100

    Update tf_planck2018_lite_posterior.py

--- cosmopower/likelihoods/tf_planck2018_lite/tf_planck2018_lite_posterior.py
+++ cosmopower/likelihoods/tf_planck2018_lite/tf_planck2018_lite_posterior.py
@@ -354,8 +354,8 @@ class tf_planck2018_lite_posterior:
         
 
         diff = tf.subtract(Xi_p, tf.transpose(delta_bar))
-        chi2 = tf.matmul(Psi, diff)
-        chi2 = tf.matmul(diff, chi2)
+        chi2 = tf.matmul(tf.transpose(diff), Psi)
+        chi2 = tf.matmul(chi2, diff)
 
         chi2 = tf.linalg.diag_part(chi2)
 
