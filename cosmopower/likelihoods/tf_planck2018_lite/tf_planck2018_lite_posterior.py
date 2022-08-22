36c213c8fc0ca623fd74633c0eff1c7bc43b89b3
sophuang <109146037+sophuang@users.noreply.github.com>
2022-08-22 21:57:11 +0100

    Update tf_planck2018_lite_posterior.py

--- cosmopower/likelihoods/tf_planck2018_lite/tf_planck2018_lite_posterior.py
+++ cosmopower/likelihoods/tf_planck2018_lite/tf_planck2018_lite_posterior.py
@@ -289,7 +289,11 @@ class tf_planck2018_lite_posterior:
         
 
         
-        Cl_bin = tf.math.multiply(tf.gather(Cl, self.indices, axis=1), self.window_tile)
+        Cl_bin = tf.math.segment_sum( \
+        tf.transpose( \
+        tf.math.multiply(tf.gather(Cl, self.indices, axis=1), self.window_tile)), \
+        self.indices_rep)
+        
         print(np.shape(Cl_bin))
         
         # final theory prediction
